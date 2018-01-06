5416a1ff56698159c913c53eca9a042c96b0efa2
Rejab Yousaf  ____chef <35057632+rejabusaf@users.noreply.github.com>
2018-01-06 17:40:31 +0400

    Update udp-remote-Launcher.py

--- udp-remote-Launcher.py
+++ udp-remote-Launcher.py
@@ -25,12 +25,12 @@ class Launcher:
 
     def openApp(self, receivedBin):
         if receivedBin == self.UDPBin:
-            print(datetime.datetime.now(), ":\t", "Wake-on-LAN received for ApplicationID:", self.ApplicationID)
+            print(datetime.datetime.now(), ":", "Matching UDP Packet received for ApplicationID:", self.ApplicationID)
             if self.isTaskRunning():
-                print(datetime.datetime.now(), ":\t", self.ApplicationParameters + 'is already running')
+                print(datetime.datetime.now(), ":", self.ApplicationParameters + 'is already running')
                 return False
             else:
-                print(datetime.datetime.now(), ":\t", 'Starting:', self.ApplicationParameters)
+                print(datetime.datetime.now(), ":", 'Starting:', self.ApplicationParameters)
                 subprocess.call([self.ApplicationPath + "\\" + self.ApplicationParameters], 0)
                 return True
 
@@ -51,14 +51,14 @@ class Launcher:
                 dead.add(ref)
         cls._instances -= dead
 
-    def __repr__(self):
-        return "Launcher('{}','{}','{}','{}'".format(self.ApplicationID, self.ApplicationPath, self.ApplicationParameters, self.UDPBin)
+    def __str__(self):
+        return "Button #{} - ApplicationPath: {} - ApplicationParameters: {} - UDPBin: {}".format(self.ApplicationID, self.ApplicationPath, self.ApplicationParameters, self.UDPBin)
 
 
 def getCapturedString():
     wOLServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     wOLServer.bind(("", wolPort))
-    print(datetime.datetime.now(), ":\t", "Listening on 0.0.0.0 for Wake-on-LAN on port", wolPort, '(UDP)')
+    print(datetime.datetime.now(), ":", "Listening on 0.0.0.0 for Matching UDP Packets on port", wolPort, '(UDP)')
     wOLData = wOLServer.recv(1024)
     wOLServer.shutdown(0)
     wOLDatastring = wOLData.decode("utf-8")
@@ -71,18 +71,18 @@ AppInstanceCounter = 1
 Apps = []
 
 if os.path.isfile('./'+configFileName):
-    print("Config File found: " + configFileName + ". Loading Buttons")
+    print(datetime.datetime.now(), ":", "Config File found: " + configFileName + ". Loading Buttons")
     Apps = pickle.load(open(configFileName, "rb"))
-    print(Apps)
 else:
-    print("No Config File found: config.raw")
-    print(datetime.datetime.now(), "Setup follows")
-    print(datetime.datetime.now(), "--------------------------------------------------------\n")
+    print(datetime.datetime.now(), ":", "No Config File found: config.raw")
+    print(datetime.datetime.now(), ":", "Setup follows")
+    print(datetime.datetime.now(), ":", "--------------------------------------------------------\n")
     for i in range(0,8):
-        print(datetime.datetime.now(), "Button No:", AppInstanceCounter, ": setup follows:")
+        print(datetime.datetime.now(), ":", "Button #" + str(AppInstanceCounter),"setup follows:")
         inApplicationPath = input("Enter Command/Path >>>>>: ")
         Apps.append(1)
         Apps[i] = Launcher(inApplicationPath)
+        print(datetime.datetime.now(), ":", "Action saved:",str(Apps[i]))
     pickle.dump(Apps, open(configFileName, "wb"), pickle.HIGHEST_PROTOCOL)
 
 run = True
@@ -91,4 +91,3 @@ while run:
     capturedStr = getCapturedString()
     for i in range(0,8):
         Apps[i].openApp(capturedStr)
-
